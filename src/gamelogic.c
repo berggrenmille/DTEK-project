0b64e576e65290c28ee194bcbf63e261949ec992
Michael Ask <micask@kth.se>
2020-12-06 18:03:42 +0100

    Formatting and fixes

--- src/gamelogic.c
+++ src/gamelogic.c
@@ -7,8 +7,8 @@ void update_logic(int dTime){
 	float speed = (float)dTime * 5 / 100.0;	
 	if(get_btns(1)){
 		player.x += speed;
-	        if(player.y < 0)
- 			player.y = 0;			
+	        if(player.x > 128 - player.w)
+ 			player.x = 128 - player.w;
 	}
 	if(get_btns(2)){
 		player.x -= speed;
@@ -17,12 +17,12 @@ void update_logic(int dTime){
 	}
 	if(get_btns(3)){
 		player.y += speed;
-	        if(player.x > 128 - player.w)
- 			player.x = 128 - player.w;			
+	        if(player.y > 32 - player.h)
+ 			player.y = 32 - player.h;
 	}
 	if(get_btns(4)){
 		player.y -= speed;
-	        if(player.y > 32 - player.h)
- 			player.y = 32 - player.h;			
+	        if(player.y < 0)
+ 			player.y = 0;
 	}
 }
