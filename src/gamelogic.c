18accf19b58506f9582c5e5095e2e965365d022a
Michael Ask <micask@kth.se>
2020-12-06 19:19:35 +0100

    Added logic for moving and spawning enemies

--- src/gamelogic.c
+++ src/gamelogic.c
@@ -2,9 +2,12 @@
 #include "entity.h"
 
 extern struct entity player;
+extern int score;
+int interval = 0;
 
 void update_logic(int dTime){
 	float speed = (float)dTime * 5 / 100.0;	
+	interval += dTime;
 	if(get_btns(1)){
 		player.x += speed;
 	        if(player.x > 128 - player.w)
@@ -24,5 +27,20 @@ void update_logic(int dTime){
 		player.y -= speed;
 	        if(player.y < 0)
  			player.y = 0;
+	}
+
+	//Move the enemies
+	int i;
+	for(i = 0; i < 10; i++)
+		enemies[i].x -= speed*(score+1000)/10000;
+
+	//Try to randomly spawn an enemy after 2 seconds
+	if(interval > 2000){
+		interval = 0;
+		int randEnemy = rand() % 10;
+		if(enemies[randEnemy].x + enemies[randEnemy].w < 0){
+			enemies[randEnemy].x = 140;
+			enemies[randEnemy].y = rand() % (33 - enemies[randEnemy].h);
+		}
 	}
 }
